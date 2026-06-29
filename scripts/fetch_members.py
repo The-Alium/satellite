af8ec1acf48833f41a91daba56511e04559dd4ad
Unknown Developer <olegis019@gmail.com>
2026-06-30 02:01:46 +0400

    portraits

--- scripts/fetch_members.py
+++ scripts/fetch_members.py
@@ -1,28 +1,37 @@
-import os
 import json
+import os
+from pathlib import Path
+
 import requests
 import xmltodict
 
 GROUP = os.environ["STEAM_GROUP"]
 BASE_URL = f"https://steamcommunity.com/groups/{GROUP}/memberslistxml/?xml=1"
 
+
 def main():
-    print( "Getting data from Steam..." )
-    r = requests.get( BASE_URL, timeout = 10 )
+    print("Getting data from Steam...")
+    r = requests.get(BASE_URL, timeout=10)
 
     if r.status_code == 404:
-        raise RuntimeError( "Group is unavailable or does not exist." )
+        raise RuntimeError("Group is unavailable or does not exist.")
 
     r.raise_for_status()
 
-    with open( "members.json", "w", encoding = "utf-8" ) as f:
-        json.dump(
-            xmltodict.parse( r.content ),
-            f,
-            indent = 2
-        )
+    with open("members.json", "w", encoding="utf-8") as f:
+        json.dump(xmltodict.parse(r.content), f, indent=2)
 
-    print( "Done." )
+    files: list[str] = []
+
+    for path in Path("./resources/portraits").rglob("*"):
+        if path.is_file():
+            files.append(str(path.relative_to("./resources/portraits")))
+
+    with open("portraits.json", "w", encoding="utf-8") as f:
+        json.dump(files, f, indent=4, ensure_ascii=False)
+
+    print("Done.")
+
 
 if __name__ == "__main__":
     main()
